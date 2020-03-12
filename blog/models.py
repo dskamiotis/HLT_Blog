c62487949e63036e88454d0fd773c3e5d91518a7
D Skamkiotis <dskamiotis@yahoo.co.uk>
2020-03-12 17:25:48 +0000

    Added new hTML files

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,14 @@
+
 from django.db import models
 from django.utils import timezone
-# from django.core.urlresolvers import reverse
 from django.urls import reverse
-# Create your models here.
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateField(default=timezone.now())
-    published_date = models.DateField(blank=True, null=True)
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -26,18 +25,16 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments',on_delete=models.CASCADE)
-    author = models.CharField(max_length=30)
+    author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
     def approved(self):
         self.approved_comment = True
         self.save()
-
     def get_absolute_url(self):
         return reverse('post_list')
-
     def __str__(self):
         return self.text
 
